e380ca92751a9252dcf916a1ebf6613c3d8c0c19
BryantCR <bryancvargas20@gmail.com>
2021-09-14 09:26:21 -0600

    Adding Changes

---
+++ Part II/js/index.js
@@ -0,0 +1,20 @@
+function changeValue1(){
+    let valueNum = document.querySelector('.likesValue1');
+    let plusValue = parseInt(valueNum.innerHTML);
+    plusValue += 1;
+    valueNum.innerHTML = plusValue;
+}
+
+function changeValue2(){
+    let valueNum = document.querySelector('.likesValue2');
+    let plusValue = parseInt(valueNum.innerHTML);
+    plusValue += 1;
+    valueNum.innerHTML = plusValue;
+}
+
+function changeValue3(){
+    let valueNum = document.querySelector('.likesValue3');
+    let plusValue = parseInt(valueNum.innerHTML);
+    plusValue += 1;
+    valueNum.innerHTML = plusValue;
+}
