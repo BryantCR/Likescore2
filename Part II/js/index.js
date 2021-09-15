bb9eca5d9ca8bf65d3b6d453562b1ac88af4c2fc
Bryan Cascante Vargas <89356007+BryantCR@users.noreply.github.com>
2021-09-15 12:34:20 -0600

    Update index.js
    Function increase

--- Part II/js/index.js
+++ Part II/js/index.js
@@ -18,3 +18,19 @@ function changeValue3(){
     plusValue += 1;
     valueNum.innerHTML = plusValue;
 }
+/*
+
+function increaselikes(event){
+    console.log(event);
+    let currentButton = event.target;
+    let numberLikes = parseInt(currentButton.closest('.dessertLikes').querySelector('.numberLikes').textContent);
+    numberLikes ++;
+    currentButton.closest('.dessertLikes').querySelector('.numberLikes').textContent = numberLikes;
+}
+
+let heartButton = document.querySelectorAll('.heartImage');
+for(let i = 0; i < heartButton.length; i++){
+    heartButton[i].addEventListener("click", increaselikes);
+}
+
+*/
